7a9a4ee5ccd3d7167b4a0b7b051c281169d03478
gundrm <rachelm.gundy@gmail.com>
2019-06-27 16:19:35 -0700

    commit v1.0 trivia
    reset not functioning properly

---
+++ assets/trivia.js
@@ -0,0 +1,29 @@
+
+$("#start").on("click", startTrivia); //start click quiz
+$("#restart").on("click", restartTrivia); // restart click quiz
+
+//for loop with timer - that can relate back to start that will loop x number of times relating to question #
+
+
+
+//object containing - question variables
+var triviaQuestions = [
+    {
+      question: "Made from Pig",
+      responses: "hamburger, bacon"
+      answer: "bacon",
+      image: "https://www.placecage.com/g/200/300"
+    },
+    {
+      question: "Slowest Time to Travel",
+      responses: "light-speed, rush-hour"
+      answer: "rush-hour",
+      image: "http://placekitten.com/g/200/300"
+    },
+  ];
+  
+
+
+
+
+
